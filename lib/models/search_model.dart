506bf9a31f4e6d0f250bebad32a3968d8dfa8601
Darshpreet2000 <dsingh200021@gmail.com>
2020-06-10 19:31:37 +0530

    Modified Constructor to have optional arguements

--- lib/models/search_model.dart
+++ lib/models/search_model.dart
@@ -1,7 +1,6 @@
 class SearchModel{
   String desciption,charge,category,name;
 
-  SearchModel(this.desciption, this.charge, this.category, this.name);
-  SearchModel.withoutName(this.desciption, this.charge, this.category);
+  SearchModel(this.desciption, this.charge, this.category, [this.name]);
 
 }
--- lib/screens/view_cdm/components/cdm.dart
+++ lib/screens/view_cdm/components/cdm.dart
@@ -4,11 +4,11 @@ import 'package:curativecare/models/search_model.dart';
 import 'package:flutter/material.dart';
 
 List<SearchModel> procedures_list = [
-  SearchModel.withoutName('ROOM/BED: Newborn Routine', '3172.4', 'Standard'),
-  SearchModel.withoutName('Lung Transplant', '4000', 'DRG'),
-  SearchModel.withoutName('Heart Failure', '5800', 'DRG'),
-  SearchModel.withoutName('Kidney Transplant Paired Donor', '20008', 'DRG'),
-  SearchModel.withoutName('ALTEPLASE 100MG SOLR 1 EACH VIAL', '3801.76', 'Pharmacy')
+  SearchModel('ROOM/BED: Newborn Routine', '3172.4', 'Standard'),
+  SearchModel('Lung Transplant', '4000', 'DRG'),
+  SearchModel('Heart Failure', '5800', 'DRG'),
+  SearchModel('Kidney Transplant Paired Donor', '20008', 'DRG'),
+  SearchModel('ALTEPLASE 100MG SOLR 1 EACH VIAL', '3801.76', 'Pharmacy')
 ];
 
 class CDM extends StatefulWidget {
