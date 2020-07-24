e9ad4dc22cec97a8641e4b548aca3caadebbee9f
Darshpreet Singh <dsingh200021@gmail.com>
2020-07-24 20:24:22 +0530

    Update body.dart

--- lib/screens/saved/components/body.dart
+++ lib/screens/saved/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:curativecare/bloc/download_cdm_bloc/download_cdm_list/download_cdm_bloc.dart';
 import 'package:curativecare/bloc/download_cdm_bloc/download_cdm_list/download_cdm_state.dart';
+import 'package:curativecare/bloc/download_cdm_bloc/download_cdm_progress/bloc.dart';
 import 'package:curativecare/bloc/saved_screen_bloc/saved_screen_bloc.dart';
 import 'package:curativecare/bloc/saved_screen_bloc/saved_screen_event.dart';
 import 'package:curativecare/bloc/saved_screen_bloc/saved_screen_state.dart';
@@ -10,7 +11,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocListener<DownloadCdmBloc, DownloadCdmState>(
+    return BlocListener<DownloadFileButtonBloc, DownloadFileButtonState>(
+      listener: (BuildContext context, DownloadFileButtonState state) {
+        if (state is DownloadButtonLoaded) {
+          context.bloc<SavedScreenBloc>().add(LoadSavedData());
+        } else if (state is  DownloadFileButtonError) {
+          context.bloc<SavedScreenBloc>().add(ShowNoDataFound());
+        }
+      },
+
+      child:BlocListener<DownloadCdmBloc, DownloadCdmState>(
       listener: (BuildContext context, DownloadCdmState state) {
         if (state is LoadedState) {
           context.bloc<SavedScreenBloc>().add(LoadSavedData());
@@ -39,6 +49,7 @@ class Body extends StatelessWidget {
           }
         },
       ),
+    )
     );
   }
 }
